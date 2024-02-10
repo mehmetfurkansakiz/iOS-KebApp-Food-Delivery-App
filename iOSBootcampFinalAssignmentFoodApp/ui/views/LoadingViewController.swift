97503fbfb189344311c6c3e813548af955a5f7e0
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2024-02-10 03:11:52 +0300

    Loading Screen Added.

---
+++ iOSBootcampFinalAssignmentFoodApp/ui/views/LoadingViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LoadingViewController.swift
+//  iOSBootcampFinalAssignmentFoodApp
+//
+//  Created by furkan sakız on 8.02.2024.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+    
+    var loadingActivityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        
+        indicator.style = .large
+        indicator.color = .white
+        indicator.startAnimating()
+        indicator.autoresizingMask = [
+            .flexibleLeftMargin, .flexibleRightMargin,
+            .flexibleTopMargin, .flexibleBottomMargin
+        ]
+            
+        return indicator
+    }()
+    
+    var blurEffectView: UIVisualEffectView = {
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        
+        blurEffectView.alpha = 0.8
+        blurEffectView.autoresizingMask = [
+            .flexibleWidth, .flexibleHeight
+        ]
+        
+        return blurEffectView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        
+        blurEffectView.frame = self.view.bounds
+        view.insertSubview(blurEffectView, at: 0)
+        
+        loadingActivityIndicator.center = CGPoint(
+            x: view.bounds.midX,
+            y: view.bounds.midY
+        )
+        view.addSubview(loadingActivityIndicator)
+
+    }
+}
