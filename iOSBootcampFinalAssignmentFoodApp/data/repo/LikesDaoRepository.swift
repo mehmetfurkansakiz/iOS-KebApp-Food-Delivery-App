0bda827a318e05ee4e277bbdad16860c13662dd0
mehmetfurkansakiz <mehmetfurkansakiz@gmail.com>
2024-03-07 02:28:39 +0300

    Fixed firebase directory for Likes repository.

--- iOSBootcampFinalAssignmentFoodApp/data/repo/LikesDaoRepository.swift
+++ iOSBootcampFinalAssignmentFoodApp/data/repo/LikesDaoRepository.swift
@@ -22,41 +22,61 @@ class LikesDaoRepository {
     }
     
     func setLike(viewController: UIViewController, food: Foods) {
+        let account = firestore.collection("Accounts").whereField("email", isEqualTo: Auth.auth().currentUser!.email!)
         
-        let firestoreLikes = [
-            "yemek_id": food.yemek_id!,
-            "yemek_adi" : food.yemek_adi!,
-            "yemek_fiyat" : food.yemek_fiyat!,
-            "yemek_resim_adi" : food.yemek_resim_adi!,
-            "email" : Auth.auth().currentUser!.email!,
-        ] as [String : Any]
-        
-        firestoreReference = firestore.collection("Likes").addDocument(data: firestoreLikes, completion: { error in
+        account.getDocuments { snapshot, error in
             if error != nil {
                 AlertHelper.createAlert(title: "Error", message: error?.localizedDescription ?? "Error", in: viewController)
-            } else {
-                AlertHelper.createAlert(title: "Success", message: "Food has been successfully liked.", in: viewController)
-                self.getLikes(viewController: viewController)
+            } else if let documents = snapshot?.documents {
+                if let userDocument = documents.first {
+                    let firestoreLikes = [
+                        "yemek_id": food.yemek_id!,
+                        "yemek_adi" : food.yemek_adi!,
+                        "yemek_fiyat" : food.yemek_fiyat!,
+                        "yemek_resim_adi" : food.yemek_resim_adi!,
+                        "email" : Auth.auth().currentUser!.email!,
+                    ] as [String : Any]
+                    
+                    self.firestoreReference = userDocument.reference.collection("Likes").addDocument(data: firestoreLikes, completion: { error in
+                        if error != nil {
+                            AlertHelper.createAlert(title: "Error", message: error?.localizedDescription ?? "Error", in: viewController)
+                        } else {
+                            AlertHelper.createAlert(title: "Success", message: "Food has been successfully liked.", in: viewController)
+                            self.getLikes(viewController: viewController)
+                        }
+                    })
+                }
             }
-        })
+        }
     }
     
     func undoLike(viewController: UIViewController, food: Foods) {
-        let userLikes = firestore.collection("Likes")
-            .whereField("email", isEqualTo: Auth.auth().currentUser!.email!)
-            .whereField("yemek_id", isEqualTo: food.yemek_id!)
         
-        userLikes.getDocuments { snapshot, error in
+        let account = firestore.collection("Accounts").whereField("email", isEqualTo: Auth.auth().currentUser!.email!)
+        
+        account.getDocuments { snapshot, error in
             if error != nil {
-                AlertHelper.createAlert(title: "Error", message: error?.localizedDescription ?? "Error", in: viewController)
-            } else if let document = snapshot?.documents.first{
-                let documentID = document.documentID
-                self.firestore.collection("Likes").document(documentID).delete() { error in
-                    if error != nil {
-                        AlertHelper.createAlert(title: "Error", message: error?.localizedDescription ?? "Error", in: viewController)
-                    } else {
-                        AlertHelper.createAlert(title: "Success", message: "Food has been successfully removed.", in: viewController)
-                        self.getLikes(viewController: viewController)
+                AlertHelper.createAlert(title: "Error", message: error!.localizedDescription, in: viewController)
+            } else if let documents = snapshot?.documents {
+                if let userDocument = documents.first {
+                    
+                    let likesCollection = userDocument.reference.collection("Likes")
+                        .whereField("email", isEqualTo: Auth.auth().currentUser!.email!)
+                        .whereField("yemek_id", isEqualTo: food.yemek_id!)
+                    
+                    likesCollection.getDocuments { snapshot, error in
+                        if error != nil {
+                            AlertHelper.createAlert(title: "Error", message: error?.localizedDescription ?? "Error", in: viewController)
+                        } else if let document = snapshot?.documents.first {
+                            document.reference.delete() { error in
+                                if error != nil {
+                                    AlertHelper.createAlert(title: "Error", message: error?.localizedDescription ?? "Error", in: viewController)
+                                } else {
+                                    AlertHelper.createAlert(title: "Success", message: "Food has been successfully removed.", in: viewController)
+                                    self.getLikes(viewController: viewController)
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -64,25 +84,35 @@ class LikesDaoRepository {
     }
     
     func getLikes(viewController: UIViewController) {
-        let userLikes = firestore.collection("Likes")
-            .whereField("email", isEqualTo: Auth.auth().currentUser!.email!)
+        let account = firestore.collection("Accounts").whereField("email", isEqualTo: Auth.auth().currentUser!.email!)
         
-        userLikes.getDocuments { snapshot, error in
+        account.getDocuments { snapshot, error in
             if error != nil {
                 AlertHelper.createAlert(title: "Error", message: error?.localizedDescription ?? "Error", in: viewController)
             } else if let documents = snapshot?.documents {
-                var likedFoods: [Foods] = []
-                for document in documents {
-                    do {
-                        if let like = try document.data(as: Foods?.self) {
-                            likedFoods.append(like)
+                if let userDocument = documents.first {
+                    let likesCollection = userDocument.reference.collection("Likes")
+                        .whereField("email", isEqualTo: Auth.auth().currentUser!.email!)
+                    
+                    likesCollection.getDocuments { snapshot, error in
+                        if error != nil {
+                            AlertHelper.createAlert(title: "Error", message: error?.localizedDescription ?? "Error", in: viewController)
+                        } else if let documents = snapshot?.documents {
+                            var likedFoods: [Foods] = []
+                            for document in documents {
+                                do {
+                                    if let like = try document.data(as: Foods?.self) {
+                                        likedFoods.append(like)
+                                    }
+                                } catch {
+                                    AlertHelper.createAlert(title: "Error", message: error.localizedDescription, in: viewController)
+                                }
+                            }
+                            
+                            self.likedFoodList.onNext(likedFoods)
                         }
-                    } catch {
-                        AlertHelper.createAlert(title: "Error", message: error.localizedDescription, in: viewController)
                     }
                 }
-                
-                self.likedFoodList.onNext(likedFoods)
             }
         }
     }
